00c9bf640c5187e09a1117c14f2e71445b3ca868
Mitchell Philmore <mitchellphilmore@gmail.com>
2018-11-09 21:17:52 -0500

    update

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,8 @@ let mysql = require('mysql2'),
     Sequlize = require('sequelize')
 
 let sequelize = new Sequlize('heroku_76721893dce23b8','b2da697b19cba0','036891b4',{
-    host:'us-cdbr-iron-east-01.cleardb.net/',
-    dialect:'mysql'
+    host:'mysql://b2da697b19cba0:036891b4@us-cdbr-iron-east-01.cleardb.net/heroku_76721893dce23b8?reconnect=true',
+    dialect:'clearDB'
 
 
 })
