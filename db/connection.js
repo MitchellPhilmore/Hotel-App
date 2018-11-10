0d52359c757b2e3d8137f56b17be35839100c5c1
Mitchell Philmore <mitchellphilmore@gmail.com>
2018-11-09 20:57:35 -0500

    cleardb update

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 let mysql = require('mysql2'),
     Sequlize = require('sequelize')
 
-let sequelize = new Sequlize('hotelphilmore','root','',{
-    host:'localhost',
+let sequelize = new Sequlize('hotelphilmore','b2da697b19cba0','036891b4',{
+    host:' mysql://b2da697b19cba0:036891b4@us-cdbr-iron-east-01.cleardb.net/heroku_76721893dce23b8?reconnect=true',
     dialect:'mysql'
 
 
