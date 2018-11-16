9e536ef4106e5176d987ebe32e91c1d46043410d
Mitchell Philmore <mitchellphilmore@gmail.com>
2018-11-15 20:06:40 -0500

    Set up database

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ let mysql = require('mysql2'),
 
 let sequelize = new Sequlize('hotelphilmore','b2da697b19cba0','036891b4',{
     host:'mysql://b2da697b19cba0:036891b4@us-cdbr-iron-east-01.cleardb.net/heroku_76721893dce23b8?reconnect=true',
-    dialect:'postgres'
+    dialect:'mysql'
 
 
 })
