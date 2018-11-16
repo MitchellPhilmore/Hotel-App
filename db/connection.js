ae3566c193746efcad438b7476e2b90021caa92a
Mitchell Philmore <mitchellphilmore@gmail.com>
2018-11-15 20:02:59 -0500

    Set up database

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,10 @@
 let mysql = require('mysql2'),
     Sequlize = require('sequelize')
 
-    //'heroku_76721893dce23b8',' b2da697b19cba0','036891b4', mysql://b2da697b19cba0:036891b4@us-cdbr-iron-east-01.cleardb.net/heroku_76721893dce23b8?reconnect=true'
+    
 
-let sequelize = new Sequlize('hotelphilmore','root','',{
-    host:'localhost',
-    dialect:'mysql'
+let sequelize = new Sequlize(process.env.HEROKU_POSTGRESQL_BRONZE_URL,{
+    dialect:'postgres'
 
 
 })
